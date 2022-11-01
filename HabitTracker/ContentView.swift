7759ac5ffb856eb007f7b8107b75b1fd7c17d2d1
mleers <mleers4@gmail.com>
2022-11-01 15:36:48 -0700

    Initial Commit

---
+++ HabitTracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by mitch on 11/1/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,19 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by mitch on 11/1/22.
+//  Copyright © 2022 mleers. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
